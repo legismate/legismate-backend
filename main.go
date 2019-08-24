52049aecc658053bc756ea7f7bfd6928faab8165
Andrew Garcia <runandrew@gmail.com>
2019-08-24 16:52:54 -0700

    Add middleware

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
