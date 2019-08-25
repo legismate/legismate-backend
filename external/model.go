41a4dd12d544b3d065262081af607aefa35a9bb5
Andrew Garcia <runandrew@gmail.com>
2019-08-25 10:49:31 -0700

    Update model

---
+++ external/model.go
@@ -0,0 +1,31 @@
+package external
+
+// Phone is a phone
+type Phone string
+
+// URL is a website URL
+type URL string
+
+// TODO: type LevelEnum string
+
+// Representative is a council member
+type Representative struct {
+	Name     string
+	Party    string
+	Phones   []Phone
+	Email    string
+	URLs     []URL
+	PhotoURL URL
+	Office   Office
+}
+
+// Office is the type of office a representative holds
+type Office struct {
+	LevelEnum string // CITY
+	Name      string // Seattle District 6
+}
+
+// District is a local division of government
+type District struct {
+	Name string
+}
