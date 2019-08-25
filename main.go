ca642f15e7a59f0d885ecea1dcc2bbf164106526
Jessi Shank <jessishank1@gmail.com>
2019-08-24 17:30:54 -0700

    stuuubbbbbbs for routes

--- main.go
+++ main.go
@@ -2,23 +2,9 @@ package main
 
 import (
 	"net/http"
-	"time"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(60 * time.Second))
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	r := getRouter()
 	http.ListenAndServe(":3000", r)
 }
---
+++ router.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func getRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/districts", func(r chi.Router) { // everything in districts will take address
+		r.Get("/", getDistrictByLocation)
+		r.Get("/representatives", getRepsByDistrict) // query params for level adn district number
+		r.Get("/deadlines", getDeadlinesByDistrict)
+	})
+
+	r.Route("/representatives", func(r chi.Router) {
+		r.Get("/{repId}", getRepById)
+		r.Get("/{repId}/history", getRepBillHistory)
+	})
+
+	r.Route("/bills", func(r chi.Router) {
+		r.Get("/", getBillsByLevel)
+		r.Get("/{legistarId}", getBillsByLegistarId)
+	})
+
+	// /bills/&level=city?address=______&timeframe=future&keyword=lgbtq
+
+	return r
+}
